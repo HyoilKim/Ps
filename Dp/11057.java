826ca9615692bd04dc4dda75240a03fe8aa237e1
Kim hyo il <quf9483@naver.com>
2019-07-19 04:41:47 +0900

    Dp

--- Dp/11057.java
+++ Dp/11057.java
@@ -1,22 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String args[]) {
-		Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
-		long[] arr = new long[N+1];
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int[][] dp = new int[N][1000];
 		
-		if(N == 1) {
-			System.out.print("1");
-		}else if(N == 2) {
-			System.out.print("1");
-		}else if(N >= 3) {
-			arr[1] = 1;
-			arr[2] = 1;
-			for(int i = 3; i <= N; i++) {
-				arr[i] = arr[i-1] + arr[i-2];
+		for(int i = 0; i < 10; i++) {
+			dp[0][i] = 1;
+		}
+		
+		for(int i = 1; i < N; i++) {
+			for(int j = 0; j < 10; j++) {
+				for(int k = j; k < 10; k++) {
+					dp[i][j] += dp[i - 1][k] % 10007;
+				}
 			}
-			System.out.print(arr[N]);
-		}	
+		}
+		
+		int res = 0;
+		for(int i = 0; i < 10; i++) {
+			res += dp[N-1][i]; 
+		}
+		System.out.println(res % 10007);
 	}
 }

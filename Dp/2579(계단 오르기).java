b84e53109bab87523fffcc53112318da3af85c26
Kim hyo il <quf9483@naver.com>
2019-08-09 17:20:45 +0900

    Dp

--- Dp/2579(계단 오르기).java
+++ Dp/2579(계단 오르기).java
@@ -1,54 +1,33 @@
+package ps;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
-public class Main {
+public class Test {	
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(in.readLine());
-		int[] len = new int[N];
-		Pair[] elec = new Pair[N];
+		int[] step = new int[N];
+		int[] dp = new int[N];
 		
 		for(int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(in.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			elec[i] = new Pair(a, b);
-			len[i] = 1;
+			step[i] = Integer.parseInt(in.readLine());
 		}
-        
-		Arrays.sort(elec);
 		
-		int maxLen = len[0];
-		for(int i = 1; i < N; i++) {
-			int max = -1;
-			for(int j = 0; j < i; j++) {
-				if(elec[j].y < elec[i].y) 
-					if(len[j] > max)
-						max = len[j];			
+		for(int i = 0; i < N; i++) {	
+			if(i == 0) 
+				dp[i] = step[i];
+			else if(i == 1)	
+				dp[i] = dp[i-1] + step[i];
+			else if(i == 2) {
+				dp[i] = Math.max(step[i-2], step[i-1]);
+				dp[i] += step[i];
 			}
-			if(max != -1) len[i] += max;
-			if(len[i] > maxLen) maxLen = len[i];
+			else {
+				dp[i] = Math.max(dp[i-2], step[i-1] + dp[i-3]);
+				dp[i] += step[i];
+			}
 		}
 		
-		System.out.println(N - maxLen);
+		System.out.println(dp[N-1]);
 	}
-}
-
-class Pair implements Comparable<Pair>{
-	int x;
-	int y;
-	
-	public Pair(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	@Override
-	public int compareTo(Pair o) {
-		if(this.x > o.x) return 1;
-		else if(this.x < o.x) return -1;
-		else return 0;
-	}	
 }

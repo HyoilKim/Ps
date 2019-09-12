742e3905aae101f5fbd1e9880efa10cf54a19ba7
kimhyoil <quf9483@naver.com>
2019-09-12 11:28:30 +0900

    프로그래머스 유형별

--- Programmers/수포차(완전탐색).java
+++ Programmers/수포차(완전탐색).java
@@ -1,35 +1,31 @@
-package ps;
 import java.util.*;
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] supoja1 = {1, 2, 3, 4, 5};
+        int[] supoja2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] supoja3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score = new int[3];
 
-public class Test {
-	public static void main(String[] args) {
-		int[] answer = {1,2,3,4,5};
-		int[] supoja1 = {1, 2, 3, 4, 5};
-		int[] supoja2 = {2, 1, 2, 3, 2, 4, 2, 5};
-		int[] supoja3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-		int[] score = new int[3];
-		
-		for(int i = 0; i < answer.length; i++) {
-			int i1 = i % 5;
-			int i2 = i % 8;
-			int i3 = i % 10;
-			if(supoja1[i1] == answer[i]) score[0]++;
-			if(supoja2[i2] == answer[i]) score[1]++;
-			if(supoja3[i3] == answer[i]) score[2]++;
-		}
+        for(int i = 0; i < answers.length; i++) {
+            int i1 = i % 5;
+            int i2 = i % 8;
+            int i3 = i % 10;
+            if(supoja1[i1] == answers[i]) score[0]++;
+            if(supoja2[i2] == answers[i]) score[1]++;
+            if(supoja3[i3] == answers[i]) score[2]++;
+        }
 
-		int max = Math.max(score[0], Math.max(score[1], score[2]));
-		int resLen = 0;
-		for(int i = 0; i < 3; i++)
-			if(max == score[i])
-				resLen++;
-        
-        int[] res = new int[resLen];
-		for(int i = 0, j = 0; i < 3; i++) {
-			if(max == score[i]) {
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
+        int tmp = 0;
+        for(int i = 0; i < 3; i++)
+            if(max == score[i])
+                tmp++;
+
+        int[] res = new int[tmp];
+        for(int i = 0, j = 0; i < 3; i++)
+            if(max == score[i])
                 res[j++] = i+1;
-			}
-		}
-	}
-}
 
+        return res;
+    }
+}
--- Programmers/탑(스택).java
+++ Programmers/탑(스택).java
@@ -1,23 +1,16 @@
-package ps;
-import java.util.*;
+class Solution {
+    public int[] solution(int[] heights) {
+        int len = heights.length;
+        int[] res = new int[len];
 
-public class Test {
-	public static void main(String[] args) {
-		int[] heights =  {3,9,9,3,5,7,2};
-		int len = heights.length;
-		int[] res = new int[len];
-		
-		for(int i = len-1 ; i >= 0; i--) {
-			for(int j = i; j >= 0; j--) {
-				if(heights[i] < heights[j]) {
-					res[i] = j + 1;
-					break;
-				}
-			}
-		}
-		
-		for(int i = 0; i < len; i++)
-			System.out.print(res[i] + " ");
-	}
+        for(int i = len-1 ; i >= 0; i--) {
+            for(int j = i; j >= 0; j--) {
+                if(heights[i] < heights[j]) {
+                    res[i] = j + 1;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
 }
-

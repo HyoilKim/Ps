cf69644ac09b38c638541cb330411c5a6c92eb8c
Hyoil Kim <quf9483@naver.com>
2021-01-23 03:15:29 +0900

    두 번째 이지만 이번에도 못품, 간단한 아이디어를 생각 하지 못함

--- BaekJoon/Greedy/1931(회의실배정).py
+++ BaekJoon/Greedy/1931(회의실배정).py
@@ -17,3 +17,23 @@ if __name__=="__main__":
     
     print(res)
 
+# 재시도
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+table = []
+
+for i in range(n):
+    k, v = map(int, input().split())
+    table.append((k, v))
+table = sorted(table, key = lambda x : (x[1], x[0]))
+
+pre_e = table[0][1]
+count = 1
+for s, e in table[1:]:
+    if s >= pre_e:
+        pre_e = e
+        count += 1
+
+print(count)

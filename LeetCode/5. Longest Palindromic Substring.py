480224b2cdd38f6d155de7c201fed412f6f753be
Hyoil Kim <quf9483@naver.com>
2021-03-15 02:49:09 +0900

    더 가독성 있는 best solution으로 수정

--- LeetCode/5. Longest Palindromic Substring.py
+++ LeetCode/5. Longest Palindromic Substring.py
@@ -30,21 +30,26 @@ print(time.time()-t)
 # best solution
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) <= 1:
+        def expand(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left+1:right]
+        
+        if len(s) < 2 or s==s[::-1]:
             return s
-        i,l=0,0 # 가장 긴 substring의 시작 index와 길이, 변수를 추가 해서 반복문 하나를 줄임
-        for j in range(len(s)):
-            # j로 s를 순회하면서 j를 기준으로 l+1 만큼의 길이를 가진 substring 검사
-            if s[j-l: j+1] == s[j-l: j+1][::-1]:
-                i, l = j-l, l+1
-                print(s[i: i+l])
-            # palindrom이 아니고 j>l인 경우에 i를 증가 시켜서 비교
-            # j까지 palindrom이 아닌데 j+1도 아니라면 j+2 이후도 아니다.
-            elif j > l and s[j-l-1: j+1] == s[j-l-1: j+1][::-1]:
-                i, l = j-l-1, l+2
-                print(s[i: i+l])
-        return s[i: i+l]
 
+        result = ''
+        for i in range(len(s)-1):
+            result = max(result, 
+                        expand(i,i),
+                        expand(i, i+1),
+                        key=len)
+
+        return result
+
+t = time.time()
 a = Solution()
 res = a.longestPalindrome(s)
 print(res)
+print(time.time()-t)

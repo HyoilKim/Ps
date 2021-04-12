edb9b43e01a42d61c7105d131438462daa034c8f
kimhyoil <quf9483@naver.com>
2021-04-12 17:34:47 +0900

    투포인터로 잘못 접근함, Kandane's algorithm(dp)

---
+++ LeetCode/53. Maximum Subarray.py
@@ -0,0 +1,44 @@
+# Kadane’s Algorithm
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        cursub = maxsub = nums[0]
+        
+        for num in nums[1:]:
+            cursub = max(num, cursub+num)
+            maxsub = max(maxsub, cursub)
+
+        return maxsub
+
+# divide and conquer
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        def findBestSubarray(nums, left, right):
+            # Base case - empty array.
+            if left > right:
+                return -math.inf
+
+            mid = (left + right) // 2
+            curr = best_left_sum = best_right_sum = 0
+
+            # Iterate from the middle to the beginning.
+            for i in range(mid - 1, left - 1, -1):
+                curr += nums[i]
+                best_left_sum = max(best_left_sum, curr)
+
+            # Reset curr and iterate from the middle to the end.
+            curr = 0
+            for i in range(mid + 1, right + 1):
+                curr += nums[i]
+                best_right_sum = max(best_right_sum, curr)
+
+            # The best_combined_sum uses the middle element and
+            # the best possible sum from each half.
+            best_combined_sum = nums[mid] + best_left_sum + best_right_sum
+
+            # Find the best subarray possible from both halves.
+            left_half = findBestSubarray(nums, left, mid - 1)
+            right_half = findBestSubarray(nums, mid + 1, right)
+
+            return max(best_combined_sum, left_half, right_half)
+        
+        return findBestSubarray(nums, 0, len(nums) - 1)

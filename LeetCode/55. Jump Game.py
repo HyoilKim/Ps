c3c250df86fe56cd6dd35943bc2ba57f3cddcb65
kimhyoil <quf9483@naver.com>
2021-04-13 10:54:18 +0900

    첫 번째 접근 방식에서 예외를 몰랐다면 매우 오래 걸렸을듯, d테스트 케이스 스스로 생성하는 연습하기

---
+++ LeetCode/55. Jump Game.py
@@ -0,0 +1,119 @@
+# my solution, O(n)
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # 처음 부터 점프 했을 시
+        # 현재 남은 최대 점프 높이
+        if len(nums) == 1:
+            return True
+        
+        dp = [0]*len(nums)
+        dp[0] = nums[0]
+        for i in range(1, len(nums)-1):
+            dp[i] = max(dp[i-1]-1, nums[i])
+       
+        for j in range(len(dp)-1):
+            if dp[j] == 0:
+                return False
+        return True
+
+# ineffcient solution
+# backtrack, O(2^N) - O(N^N)?
+public class Solution {
+    public boolean canJumpFromPosition(int position, int[] nums) {
+        if (position == nums.length - 1) {
+            return true;
+        }
+
+        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+            if (canJumpFromPosition(nextPosition, nums)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean canJump(int[] nums) {
+        return canJumpFromPosition(0, nums);
+    }
+}
+
+# dp, Top-down
+# O(n^2): nums[i] and memo can be at most n
+enum Index {
+    GOOD, BAD, UNKNOWN
+}
+
+public class Solution {
+    Index[] memo;
+
+    public boolean canJumpFromPosition(int position, int[] nums) {
+        # memo
+        if (memo[position] != Index.UNKNOWN) {
+            return memo[position] == Index.GOOD ? true : false;
+        }
+
+        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+            if (canJumpFromPosition(nextPosition, nums)) {
+                memo[position] = Index.GOOD;
+                return true;
+            }
+        }
+
+        memo[position] = Index.BAD;
+        return false;
+    }
+
+    public boolean canJump(int[] nums) {
+        memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[memo.length - 1] = Index.GOOD;
+        return canJumpFromPosition(0, nums);
+    }
+}
+
+# dp, Bottom-up
+# O(n^2)
+# better performance, no stack overhead(no recursion)
+enum Index {
+    GOOD, BAD, UNKNOWN
+}
+
+public class Solution {
+    public boolean canJump(int[] nums) {
+        Index[] memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[memo.length - 1] = Index.GOOD;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int furthestJump = Math.min(i + nums[i], nums.length - 1);
+            for (int j = i + 1; j <= furthestJump; j++) {
+                if (memo[j] == Index.GOOD) {
+                    memo[i] = Index.GOOD;
+                    break;
+                }
+            }
+        }
+
+        return memo[0] == Index.GOOD;
+    }
+}
+
+# Greedy, O(n)
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+}

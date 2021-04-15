72f8e1690fc34d8fd37649a6dd4b474a81cef4a5
kimhyoil <quf9483@naver.com>
2021-04-15 10:39:23 +0900

    add best solution

--- LeetCode/62. Unique Paths.py
+++ LeetCode/62. Unique Paths.py
@@ -30,3 +30,13 @@ class Solution:
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
         
         return dp[-1][-1]
+
+# best solution
+# space-complexity: O(n)
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] = dp[j - 1] + dp[j]
+        return dp[-1]

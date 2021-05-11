ad45e13f6160a2e4916a4269a776d71c4894fe3d
hyoilkim <quf9483@naver.com>
2021-05-11 12:49:04 +0900

    효율성 테스트: info를 '-'를 포함하여 재구성하고 점수를 binary search로 찾기

---
+++ Kakao/순위 검색.py
@@ -0,0 +1,31 @@
+# my solution
+from collections import defaultdict
+from bisect import bisect_left
+from itertools import combinations
+
+def solution(info, query):
+    info = [i.split() for i in info]
+    info_dict = defaultdict(list)
+    for _info in info:
+        data, score = _info[:-1], int(_info[-1])
+        for i in range(5):
+            for comb in combinations([0,1,2,3], i):
+                _data = data[:]
+                for k in j:
+                    _data[k] = '-'
+                info_dict[''.join(_data)].append(score)
+                
+    for k in info_dict:
+        info_dict[k].sort() 
+        
+    answer = []
+    for q in query:
+        q = q.replace(" and", "").split()
+        _q, score = ''.join(q[:-1]), int(q[-1])
+        if info_dict[_q]:
+            idx = bisect_left(info_dict[_q], score)
+            answer.append(len(info_dict[_q]) - idx)
+        else:
+            answer.append(0)
+    
+    return answer

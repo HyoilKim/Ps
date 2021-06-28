f652f5e7ca20251e6ef3dfe853ccf8d376bae35c
hyoilkim <quf9483@naver.com>
2021-06-28 12:42:14 +0900

    반복되는 수가 2자리수 이상인 경우를 놓침

---
+++ Kakao/문자열 압축.py
@@ -0,0 +1,18 @@
+def solution(s):
+    min_len = len(s)
+    for n in range(1,(len(s)//2)+1):
+        length = 0
+        i = 0
+        while i < len(s):
+            token = s[i:n+i]
+            iter = -1
+            while token == s[i:n+i]:
+                i += n
+                iter += 1
+                
+            if iter > 0:
+                length += n+len(str(iter+1))
+            else:
+                length += len(token)
+        min_len = min(min_len, length)
+    return min_len

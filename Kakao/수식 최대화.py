379dac8adc84b2467655687a51ea0b61437c6e4b
Hyoil Kim <quf9483@naver.com>
2021-07-05 16:59:17 +0900

    recursion 풀이 추가

--- Kakao/수식 최대화.py
+++ Kakao/수식 최대화.py
@@ -17,3 +17,26 @@ def solution(expression):
         answer = max(answer, abs(int(_ex[0])))
         
     return answer
+
+# recursion
+from itertools import permutations
+def calc(priority, n, expression):
+    if n == 2:
+        return str(eval(expression))
+    if priority[n] == '*':
+        res = eval('*'.join([calc(priority, n + 1, e) for e in expression.split('*')]))
+    if priority[n] == '+':
+        res = eval('+'.join([calc(priority, n + 1, e) for e in expression.split('+')]))
+    if priority[n] == '-':
+        res = eval('-'.join([calc(priority, n + 1, e) for e in expression.split('-')]))
+    return str(res)
+
+
+def solution(expression):
+    answer = 0
+    priorities = (list(permutations(['*','-','+'], 3)))
+    for priority in priorities:
+        res = int(calc(priority, 0, expression))
+        answer = max(answer, abs(res))
+
+    return answer

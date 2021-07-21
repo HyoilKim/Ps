ab258bd7a7c25bbf9325fb72034fdf475a51ddf8
Hyoil Kim <quf9483@naver.com>
2021-07-21 16:17:36 +0900

    second try pass

--- LeetCode/3. Longest Substring Without Repeating Characters.py
+++ LeetCode/3. Longest Substring Without Repeating Characters.py
@@ -1,22 +1,22 @@
 from collections import defaultdict
 
-# def lengthOfLongestSubstring(s):
-#     longest_str = ''
-#     for i in range(len(s)):
-#         substr = s[i]
-#         repeat = defaultdict(int)
-#         repeat[s[i]] = 1
+def lengthOfLongestSubstring(s):
+    longest_str = ''
+    for i in range(len(s)):
+        substr = s[i]
+        repeat = defaultdict(int)
+        repeat[s[i]] = 1
 
-#         for j in range(i+1, len(s)):
-#             if repeat[s[j]]:
-#                 break
-#             substr += s[j]
-#             repeat[s[j]] = 1
+        for j in range(i+1, len(s)):
+            if repeat[s[j]]:
+                break
+            substr += s[j]
+            repeat[s[j]] = 1
 
-#         if len(longest_str) < len(substr):
-#             longest_str = substr    
+        if len(longest_str) < len(substr):
+            longest_str = substr    
     
-#     # return longest_str
+    return longest_str
 
 # best solution 
 def lengthOfLongestSubstring(s):
@@ -35,3 +35,22 @@ def lengthOfLongestSubstring(s):
 s = "pwwkew"
 res = lengthOfLongestSubstring(s)
 print(res)
+
+# second try
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = r = 0
+        max_len = 0
+        visited = dict()
+        while r < len(s):
+            if s[r] in visited:
+                visited[s[l]] -= 1
+                if visited[s[l]] == 0:
+                    del visited[s[l]]
+                l += 1
+            else:
+                visited[s[r]] = 1
+                r += 1
+            max_len = max(r-l, max_len)
+            
+        return max_len

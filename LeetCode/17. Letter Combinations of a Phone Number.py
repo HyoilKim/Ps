4631afe29635b753ebde7a88b53994e48aaaf8c3
Hyoil Kim <quf9483@naver.com>
2021-07-22 16:53:56 +0900

    second try fail, 가변 길이 recursion 으로 해결

--- LeetCode/17. Letter Combinations of a Phone Number.py
+++ LeetCode/17. Letter Combinations of a Phone Number.py
@@ -1,5 +1,5 @@
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
+    def letterCombinations(self, digits):
         if digits == "":
             return []
         
@@ -17,3 +17,20 @@ class Solution:
 
         backtracking(-1, "")
         return result
+    
+# another solution
+def letterCombinations(self, digits):
+    mapping = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', 
+                '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+    if len(digits) == 0:
+        return []
+    if len(digits) == 1:
+        return list(mapping[digits[0]])
+    prev = self.letterCombinations(digits[:-1])
+    additional = mapping[digits[-1]]
+    return [s + c for s in prev for c in additional]
+
+    # after
+    after = self.letterCombinations(digits[1:])
+    additional = mapping[digits[0]]
+    return [c+s for s in after for c in additional]

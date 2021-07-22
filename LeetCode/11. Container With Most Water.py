499de2a619cca9533831deda5272800b6e16b0b9
Hyoil Kim <quf9483@naver.com>
2021-07-22 13:48:31 +0900

    second try fail, 접근 방법 고민하기 + dp에 꽂히지 말 것

--- LeetCode/11. Container With Most Water.py
+++ LeetCode/11. Container With Most Water.py
@@ -1,18 +1,21 @@
-class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        l = 0
-        r = len(height)-1
-        max_area = -1
-        
-        while l < r:
-            h = height[l] if height[l] < height[r] else height[r]
-            area = (r-l) * h
-            if area > max_area:
-                max_area = area
+'''
+idea
+1. The widest container (using first and last line) is a good candidate, because of its width. Its water level is the height of the smaller one of first and last line.
+2. All other containers are less wide and thus would need a higher water level in order to hold more water.
+3. The smaller one of first and last line doesn't support a higher water level and can thus be safely removed from further consideration.
 
-            if height[l] < height[r]:
-                l += 1
+time - O(n)
+space - O(1)
+'''
+# best solution
+class Solution:
+    def maxArea(self, height):
+        i, j = 0, len(height) - 1
+        water = 0
+        while i < j:
+            water = max(water, (j - i) * min(height[i], height[j]))
+            if height[i] < height[j]:
+                i += 1
             else:
-                r -= 1
-                
-        return max_area
+                j -= 1
+        return water

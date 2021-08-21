b58a161aa137df7a672344cdf6e8deec8c865641
Hyoil Kim <quf9483@naver.com>
2021-08-21 19:03:59 +0900

    dp 점화식은 직관적으로 찾으려 하지 말고, 케이스 모두 해보고 규칙을 찾자 + dp에 문자를 넣음

---
+++ BaekJoon/Dp/9252(LCS 2).py
@@ -0,0 +1,17 @@
+s1 = list(map(str, input()))
+s2 = list(map(str, input()))
+n1, n2 = len(s1), len(s2)
+dy = [['']*(n2+1) for _ in range(n1+1)]
+for i in range(1, n1+1):
+    for j in range(1, n2+1):
+        if s1[i-1] == s2[j-1]:
+            dy[i][j] = dy[i-1][j-1]+s1[i-1]
+        else:
+            if len(dy[i-1][j]) > len(dy[i][j-1]):
+                dy[i][j] = dy[i-1][j]
+            else:
+                dy[i][j] = dy[i][j-1]
+
+
+print(len(dy[-1][-1]))
+print(dy[-1][-1])

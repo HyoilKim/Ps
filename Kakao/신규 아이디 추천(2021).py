789083b2cffaee967f70e217860ec36c54c1a6e4
Hyoil Kim <quf9483@naver.com>
2021-08-25 21:57:51 +0900

    second try: success, 풀이 외운듯

--- Kakao/신규 아이디 추천(2021).py
+++ Kakao/신규 아이디 추천(2021).py
@@ -58,3 +58,41 @@ def solution(new_id):
     return answer
 
 print(solution(input()))
+
+
+# second try
+def solution(new_id):
+    # 1단계
+    new_id = new_id.lower()
+    
+    # 2단계
+    tmp = ''
+    for ch in new_id:
+        if ch.isdigit() or ch.isalpha() or ch in '-_.':
+            tmp += ch
+    new_id = tmp
+    
+    # 3단계
+    while '..' in new_id:
+        new_id = new_id.replace('..', '.')
+        
+    # 4단계
+    if new_id and new_id[0] == '.':
+        new_id = new_id[1:]
+    if new_id and new_id[-1] == '.':
+        new_id = new_id[:-1]
+        
+    # 5단계
+    if new_id == '':
+        new_id = 'a'
+        
+    # 6단계
+    new_id = new_id[:15]
+    if new_id[-1] == '.':
+        new_id = new_id[:-1]
+    
+    # 7단계
+    if len(new_id) <= 2:
+        new_id += new_id[-1]*(3-len(new_id))
+    
+    return new_id

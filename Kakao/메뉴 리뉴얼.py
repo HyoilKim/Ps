de8d7e4ff4426c2f38629ed38b582b89426bb94a
Hyoil Kim <quf9483@naver.com>
2021-08-25 21:58:09 +0900

    second try: success, 처음에 막혔던 부분 해결 해서 뿌듯

--- Kakao/메뉴 리뉴얼.py
+++ Kakao/메뉴 리뉴얼.py
@@ -35,3 +35,31 @@ def solution(orders, course):
         result += [ k for k, v in most_ordered if v > 1 and v == most_ordered[0][1] ]
 
     return [ ''.join(v) for v in sorted(result) ]
+
+# second try
+from collections import defaultdict
+from itertools import combinations, permutations
+def solution(orders, course):         
+    # 2명 이상의 고객에게 주문된 조합이어야 한다
+    # 음식 조합이 가장 높은 빈도를 우선으로 코스로 구성한다
+    candi = defaultdict(int) # {메뉴구성:빈도}
+    
+    for order in orders:
+        order = ''.join(sorted(order))
+        for n in range(2, len(order)+1):
+            for menu in [''.join(x) for x in combinations(order, n)]:
+                candi[menu] += 1
+                
+    result = []
+    for n in course:
+        tmp = defaultdict(list)
+        max_freq = 0
+        for menu, freq in candi.items():
+            if len(menu) == n:
+                tmp[freq].append(menu)
+                max_freq = max(max_freq, freq)
+        if max_freq >= 2:
+            result += tmp[max_freq]
+    
+    result.sort()
+    return result

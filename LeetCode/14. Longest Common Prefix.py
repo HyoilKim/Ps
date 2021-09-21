181c5a09db6c336a51cc7d9d0dbf69083fe59304
hyoilkim <quf9483@naver.com>
2021-09-21 10:05:12 +0900

    second try: success, don't have to check prefix

--- LeetCode/14. Longest Common Prefix.py
+++ LeetCode/14. Longest Common Prefix.py
@@ -12,3 +12,33 @@ class Solution:
                     return result
             result += ch
         return result
+    
+# second try success
+# time - O(N^3) / N: shortest string's length
+class Solution:
+    def longestCommonPrefix(self, strs):
+        strs.sort(key=len)
+        
+        for i in range(len(strs[0]), 0, -1):
+            prefix = strs[0][:i]
+            flag = True
+            for j in range(1, len(strs)):
+                if not strs[j].startswith(prefix):
+                    flag = False
+                    break
+            if flag:
+                return prefix
+        
+        return ''
+
+    # more fast solution
+    # time - O(N^2)
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        shortest = min(strs,key=len)
+        for i, ch in enumerate(shortest):
+            for other in strs:
+                if other[i] != ch:
+                    return shortest[:i]
+        return shortest 
